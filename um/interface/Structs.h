4b82bcfe2108f8450645520f0c4ad113785d043a
Alin Enachescu <alinenachescu033@gmail.com>
2018-05-08 21:21:36 +0300

    created header for helper structs

---
+++ um/interface/Structs.h
@@ -0,0 +1,45 @@
+#ifndef TCP_INSPECTION_STRUCTS
+#define TCP_INSPECTION_STRUCTS
+
+#include <cstdint>
+
+namespace TcpInspection
+{
+
+enum DataVerdict : uint32_t
+{
+	block,
+	allow,
+	drop
+};
+
+enum DataFlags : uint32_t
+{
+	normal,
+	reinjected
+};
+
+enum Direction : uint32_t
+{
+	in,
+	out
+};
+
+enum ConnectionVerdict : uint32_t
+{
+	filter,
+	unfilter,
+	drop
+};
+
+struct Ipv4Address
+{
+	uint16_t port;
+	uint32_t ip;
+};
+
+typedef uint32_t Pid;
+
+} // end TcpInspection namespace
+
+#endif
